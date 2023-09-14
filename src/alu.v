a7910aa14135d4981f5bce649d7fdf3e30834a86
jalaxy <chinajxy@outlook.com>
2023-09-14 11:44:53 +0800

    alu done

--- src/alu.v
+++ src/alu.v
@@ -1,8 +1,21 @@
 module alu(
     input [31:0] a,
     input [31:0] b,
-    input [???:0] func,
+    input [2:0] func3,
+    input [6:0] func7,
     output r,
-    output [???:0] flag
+    output [2:0] flags // { negative, zero }
 );
+    wire [31:0] r_arr[7:0];
+    assign r_arr[3'd0] = func7[5] ? a - b : a + b; // ADD/SUB
+    assign r_arr[3'd1] = a << b[4:0]; // SLL
+    assign r_arr[3'd2] = $signed(a) > $signed(b); // SLT
+    assign r_arr[3'd3] = a < b; // SLTU
+    assign r_arr[3'd4] = a ^ b; // XOR
+    assign r_arr[3'd5] = func7[5] ? $signed(a) >>> b[4:0] : a >> b[4:0]; // SRL/SRA
+    assign r_arr[3'd6] = a | b; // OR
+    assign r_arr[3'd7] = a & b; // AND
+    assign r = r_arr[func3];
+    assign flags[0] = r == 32'd0; // zero bit
+    assign negative = $signed(r) < 32'd0; // negative bit
 endmodule
--- src/gpreg.v
+++ src/gpreg.v
@@ -1,6 +1,6 @@
+`timescale 1ns/1ns
 module gpreg(
     input clk,
-    input rst,
     input rd_w,
     input [4:0] rs1_addr,
     input [4:0] rs2_addr,
@@ -11,9 +11,9 @@ module gpreg(
 );
     reg [31:0] r[0:31];
     wire clk_rd;
-    assign clk_rd = clk & (rd_w | rst);
-    assign rs1 = r[rs1_addr];
-    assign rs2 = r[rs2_addr];
-    always@(posedge clk_rd) // reset is sync mode
-        r[rd_addr] <= rst ? 32'd0 : rd;
+    assign clk_rd = clk & rd_w;
+    assign rs1 = rs1_addr == 5'd0 ? 32'd0 : r[rs1_addr];
+    assign rs2 = rs2_addr == 5'd0 ? 32'd0 : r[rs2_addr];
+    always@(posedge clk_rd)
+        r[rd_addr] <= rd;
 endmodule
