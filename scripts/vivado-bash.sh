a3fda7132785fd058d0c6cf3eb01aae0a25744c2
jalaxy <chinajxy@outlook.com>
2024-03-09 07:00:32 +0800

    modify shell scripts

--- scripts/vivado-bash.sh
+++ scripts/vivado-bash.sh
@@ -1,3 +1,3 @@
 docker run --rm -it -w /work --net=host -e DISPLAY \
-    -v .:/work -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v .:/work -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/fonts:/etc/fonts \
     -v ~/.Xauthority:/root/.Xauthority docker.io/jxy324/vivado:v0
