6ebded054752ca174b360bf034079fa0d03b9338
jalaxy <chinajxy@outlook.com>
2024-06-23 21:34:37 +0800

    fix a bug

--- src/pipeline.sv
+++ src/pipeline.sv
@@ -1038,7 +1038,7 @@ module wb_stage(input logic clk, input logic rst, output logic redir,
         if (cqpop[i] & ~cqinfo[i].rda[6]) nret++; end
     always_comb frontid = {1'b1, front[0]};
     always_comb begin nextid = 0; for (int i = 3; i >= 0; i--)
-        if (~cqpop[i] & cqvalid[i]) nextid = {1'b1, front[i]}; end
+        if (~cqpop[i] & cqvalid[i] & ~cqredir[i]) nextid = {1'b1, front[i]}; end
     always_ff @(posedge clk) if (redir) lastvalid <= 0;
         else for (int i = 0; i < 4; i++)
             if (cqpop[i]) {lastvalid, lastinfo} <= {1'b1, cqinfo[i]};
