ac38f485ed42ca27855189b98bd155e727bae908
jalaxy <chinajxy@outlook.com>
2024-06-26 20:59:09 +0800

    modify script and makefile

--- scripts/verilator-bash.sh
+++ scripts/verilator-bash.sh
@@ -1,1 +1,4 @@
-docker run --rm -it -w /work -v .:/work docker.io/jxy324/verilator:v0 /bin/bash
+export DISPLAY=localhost:10.0
+docker run --rm -it -w /work/sim/pipeline --net=host -e DISPLAY \
+    -v .:/work -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/fonts:/etc/fonts \
+    -v ~/.Xauthority:/root/.Xauthority docker.io/jxy324/riscv-sim:verilator
